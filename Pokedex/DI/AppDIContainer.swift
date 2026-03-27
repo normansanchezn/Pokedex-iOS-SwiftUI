1a442b98ff289f1cfe0b94bdc16d89b33a477b3c
Norman Sanchez <normansanchezn@gmail.com>
2026-03-26 22:21:20 -0600

    [✨] Pokemon details feature.

--- Pokedex/DI/AppDIContainer.swift
+++ Pokedex/DI/AppDIContainer.swift
@@ -11,21 +11,29 @@ import pokemon_data
 import pokemon_presentation
 
 struct AppDIContainer {
+    // MARK: - Use cases
     let fetchPokemonListUseCase: FetchPokemonListUseCase
+    let getPokemonDetailsUseCase: GetPokemonDetailsUseCase
+    
+    // MARK: - View models
     let homeViewModel: HomeViewModel
     let pokemonDetailsViewModel: PokemonDetailsViewModel
 
-    init(fetchPokemonListUseCase: FetchPokemonListUseCase) {
+    // MARK: - Init function
+    init(fetchPokemonListUseCase: FetchPokemonListUseCase, getPokemonDetailsUseCase: GetPokemonDetailsUseCase) {
         self.fetchPokemonListUseCase = fetchPokemonListUseCase
+        self.getPokemonDetailsUseCase = getPokemonDetailsUseCase
         self.homeViewModel = HomeViewModel(fetchPokemonListUseCase: fetchPokemonListUseCase)
-        self.pokemonDetailsViewModel = PokemonDetailsViewModel()
+        self.pokemonDetailsViewModel = PokemonDetailsViewModel(getPokemonDetailsUseCase: getPokemonDetailsUseCase)
     }
 
+    // MARK: - Go live App DI Container
     static func live() -> AppDIContainer {
         let fetcher = PokemonAPIStoreImpl()
         let pokemonRepository = PokemonRepositoryImpl(api: fetcher)
         let fetchPokemonListUseCase = FetchPokemonListUseCaseImpl(pokemonRepository: pokemonRepository)
+        let getPokemonDetailsUseCase = GetPokemonDetailsUseCaseImpl(pokemonRepository: pokemonRepository)
 
-        return AppDIContainer(fetchPokemonListUseCase: fetchPokemonListUseCase)
+        return AppDIContainer(fetchPokemonListUseCase: fetchPokemonListUseCase, getPokemonDetailsUseCase: getPokemonDetailsUseCase)
     }
 }
